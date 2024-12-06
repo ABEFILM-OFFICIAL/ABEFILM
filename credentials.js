59d00fd4dfbf154de9fb30b2a33a12afdf9e29c2
Chaeunwoosshi <engrmentawan@gmail.com>
2024-12-06 22:13:16 +0800

    Create credentials.js

---
+++ credentials.js
@@ -0,0 +1,21 @@
+[
+  {
+    "username": "eun",
+    "password": "woosshi",
+    "activeSession": "session_1732568426683"
+  },
+  {
+    "username": "sample",
+    "password": "sample",
+    "activeSession": "session_1732569683838"
+  },
+  {
+    "username": "geolliyaah",
+    "password": "143saranghae"
+  }, 
+ {
+    "username": "Oppasarang",
+    "password": "sarang"
+  }
+  
+]
